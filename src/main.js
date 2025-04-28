e2b6de042d9e2a1b2d1e4af5271b2b285932cdce
Kat A. Klettke <kontakt@kataklettke.dev>
2025-04-29 00:32:43 +0200

    Reworked some of the backsides on the English skill boxes. Created a german version of the page and linked it. Adjusted the language picker to the left. Made theme selection work: it actually changes colors now (albeit only links for now)

--- src/main.js
+++ src/main.js
@@ -1,24 +1,66 @@
 const colorThemes = document.querySelectorAll('[name="theme"]');
 
-//store theme
+const themeElement = document.body;
+
+/**
+ * Stores the id of the theme to local storage.
+ * @param theme string, theme ID
+ */
 const storeTheme = function (theme) {
     localStorage.setItem('theme', theme);
 }
 
-// set theme when visitor returns
-const setTheme = function() {
+/**
+ * Retrieves previously set theme id from local storage. If there is one available, restores that choice.
+ */
+const setRememberedTheme = function() {
     const activeTheme = localStorage.getItem('theme');
     colorThemes.forEach((themeOption) => {
         if (themeOption.id === activeTheme) {
             themeOption.checked = true;
         }
     });
+    setTheme(activeTheme);
 }
 
+/**
+ * Sets Event listener for each theme radio button and calls storeTheme(themeID) on click.
+ */
 colorThemes.forEach((themeOption) => {
     themeOption.addEventListener('click', () => {
         storeTheme(themeOption.id);
-    })
+        setTheme(themeOption.id);
+    });
 })
 
-document.onload = setTheme();
+const setTheme = function (theme) {
+    if (theme === 'light') {
+        themeElement.classList.remove('dark');
+        themeElement.classList.remove('blue');
+        themeElement.classList.remove('green');
+        themeElement.classList.add('light');
+    }
+    else if (theme === 'dark') {
+        themeElement.classList.remove('light');
+        themeElement.classList.remove('blue');
+        themeElement.classList.remove('green');
+        themeElement.classList.add('dark');
+    }
+    else if (theme === 'green') {
+        themeElement.classList.remove('dark');
+        themeElement.classList.remove('blue');
+        themeElement.classList.remove('light');
+        themeElement.classList.add('green');
+    }
+    else {
+        themeElement.classList.remove('dark');
+        themeElement.classList.remove('light');
+        themeElement.classList.remove('green');
+        themeElement.classList.add('blue');
+    }
+
+}
+
+document.onload = setRememberedTheme();
+
+
